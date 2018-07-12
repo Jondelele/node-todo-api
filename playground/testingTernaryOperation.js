e9992508e04cb753e182dafff480fffa1666e1df
Joona Prehti <joona.prehti@student.tut.fi>
2018-07-12 21:03:05 +0300

    Kohta 8:04, lecture 71

--- playground/testingTernaryOperation.js
+++ playground/testingTernaryOperation.js
@@ -1,3 +1,10 @@
-let age = 21;
+// let age = 21;
+//
+//
+// console.log(age >= 21 ? "You may enter." : "You may NOT enter.");
 
-console.log(age >= 21 ? "You may enter." : "You may NOT enter.");
+let age = 20;
+
+var viesti = age < 21 ? "You are Not allowed!!" : "You may enter :)";
+
+console.log(viesti);
