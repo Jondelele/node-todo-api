8b96a42b4d202706ef6f56e27ffcf3f7196c952f
Joona Prehti <joona.prehti@student.tut.fi>
2018-07-17 15:05:38 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -14,14 +15,17 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
+    console.log(req.body.text);
+
     todo.save().then((doc) => {
+        console.log(doc);
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
     });
 });
 
-// näytä kaikki todot
+
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
@@ -30,8 +34,30 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo})
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000')
 });
 
 module.exports = {app};
+
+
+
+
