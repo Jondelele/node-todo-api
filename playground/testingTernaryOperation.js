236b1c2a35ab1fd6568de65708b79d17bfbf615c
Joona Prehti <joona.prehti@student.tut.fi>
2018-07-31 16:01:00 +0300

    Add POST /users/login route

--- playground/testingTernaryOperation.js
+++ playground/testingTernaryOperation.js
@@ -1,3 +1,7 @@
-var massit = 1000;
+// var massit = 1000;
+//
+// console.log(massit > 1000 ? "Voitte tulla sisään" : "Ette voi tulla sisään");
 
-console.log(massit > 1000 ? "Voitte tulla sisään" : "Ette voi tulla sisään");
+
+palkka = 1000;
+console.log(palkka > 1000 ? "Viinaa" : "Ei viinaa lulz")
